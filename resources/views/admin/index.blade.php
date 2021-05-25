e64f0cf20eb26dab2d0f81ebcc80599bf9eb8453
Jhonatan-Cortez <jhonatancortez708@gmail.com>
2021-05-25 11:31:30 -0600

    Integrando plantilla

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+  <h1>Inicio</h1>
+@stop
+
+@section('content')
+  <p>Sistema de renta de vehiculos</p>
+@stop
+
+@section('css')
+  {{-- add your styles css --}}
+@stop
+
+@section('js')
+  {{-- add your scritp js --}}
+@stop
